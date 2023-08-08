cdf75eaf007f3145a756d9f02bb8b984ac421ed6
Cyprien Kelma <cyprien.kelma@gmail.com>
2023-08-08 23:19:14 +0200

    Add ActionListener + started display calculs

--- CalculatorJava/src/Calculator.java
+++ CalculatorJava/src/Calculator.java
@@ -8,62 +8,112 @@ import javax.swing.Timer;
 
 public class Calculator {
 
-    JFrame window = new JFrame("Calculator");
-    JPanel globalPanel = new JPanel();
-    JPanel boardPanel = new JPanel();
-    JPanel buttonPanel = new JPanel();
+  JFrame window = new JFrame("Calculator");
+  JPanel globalPanel = new JPanel();
+  JPanel boardPanel = new JPanel();
+  JPanel buttonPanel = new JPanel();
 
-    JLabel calculField = new JLabel("0");
-    JButton[] buttons = new JButton[16];
-    String[] buttonLabels = { "7", "8", "9", "/", "4", "5", "6", "*", "1", "2", "3", "-", "0", ".", "=", "+" };
+  JLabel calculField = new JLabel("0");
+  JButton[] buttons = new JButton[16];
+  String[] buttonLabels = { "7", "8", "9", "/", "4", "5", "6", "*", "1", "2", "3", "-", ".", "0", "=", "+" };
 
-    Calculator() {
+  private double currentResult = 0.0;
 
-      /* Window (JFrame) */
-      /* La fenêtre de l'application */
-      window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      window.setSize(360, 500);
-      window.setLayout(new BorderLayout());
-      window.setVisible(true);
-      window.getContentPane().setBackground(new Color(50, 50, 50));
-      window.setResizable(false);
+  Calculator() {
 
-      /* Global Panel (JPanel) */
-      /* La page dans son ensemble */
-      globalPanel.setSize(350, 490);
-      globalPanel.setLayout(new GridLayout(2, 1));
-      // TODO change to gridBagLayout + adjuste the page to put screen on top and number at the bottom
+    /* Window (JFrame) */
+    /* La fenêtre de l'application */
+    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    window.setSize(360, 450);
+    window.setLayout(new BorderLayout());
+    window.setVisible(true);
+    window.getContentPane().setBackground(new Color(242, 159, 5));
+    window.setResizable(false);
 
-      /* Board Panel (JPanel) */
-      /* "L'écran" de la calculette */
-      boardPanel.setLayout(new BorderLayout());
-      boardPanel.setBounds(14, 12, 318, 40);
+    /* Global Panel (JPanel) */
+    /* La page dans son ensemble */
+    globalPanel.setSize(350, 400);
+    globalPanel.setLayout(new GridLayout(2, 1));
 
-      /* Calcul Field (JLabel) */
-      /* Le texte qui apparait dans boardPanel */
-      calculField.setForeground(new Color(25, 120, 255));
-      calculField.setBackground(new Color(75, 90, 75));
-      calculField.setHorizontalAlignment(JLabel.RIGHT);
-      calculField.setOpaque(true);
+    /* Board Panel (JPanel) */
+    /* "L'écran" de la calculette */
+    boardPanel.setLayout(new BorderLayout());
+    boardPanel.setBackground(new Color(29, 79, 78));
+    boardPanel.setBorder(BorderFactory.createLineBorder(new Color(50, 50, 50), 3));
+    GridBagConstraints gbc = new GridBagConstraints();
+    gbc.gridx = 0;
+    gbc.gridy = 0;
+    gbc.fill = GridBagConstraints.BOTH;
+    gbc.weightx = 1.0;
+    gbc.weighty = 0.8;
+    globalPanel.add(boardPanel, gbc);
 
-      /* Button Panel (JPanel) */
-      /* La zone qui contient tout les boutons de la calculette */
-      
-      buttonPanel.setLayout(new GridLayout(5, 4, 2, 3));
-      buttonPanel.setBackground(new Color(50, 50, 50));
-      for(int i = 0; i < buttons.length; i++){ 
-        // Créer un bouton de type Jbutton (Swing library) 
-        // et le remplis avec le texte du bouton associé
-        buttons[i] = new JButton(buttonLabels[i]);
-        buttonPanel.add(buttons[i]); // Ajoute chaque bouton à la zone buttonPanel
+    /* Calcul Field (JLabel) */
+    /* Le texte qui apparait dans boardPanel */
+    calculField.setForeground(new Color(242, 203, 5));
+    calculField.setBackground(new Color(29, 79, 78));
+    calculField.setHorizontalAlignment(JLabel.RIGHT);
+    calculField.setOpaque(true);
+    calculField.setFont(new Font("Arial", Font.PLAIN, 60));
+
+    /* Button Panel (JPanel) */
+    /* La zone qui contient tout les boutons de la calculette */
+    buttonPanel.setLayout(new GridLayout(5, 4, 5, 5));
+    buttonPanel.setBackground(new Color(40, 40, 40));
+    buttonPanel.setBorder(BorderFactory.createLineBorder(new Color(50, 50, 50), 3));
+
+    for (int i = 0; i < buttons.length; i++) {
+      // Créer un bouton de type Jbutton (Swing library)
+      // et le remplis avec le texte du bouton associé
+      buttons[i] = new JButton(buttonLabels[i]);
+      buttons[i].setFont(new Font("Arial", Font.PLAIN, 30));
+
+      // Ajout d'un ActionListener pour chaque bouton (= instance de
+      // ButtonClickListener pour chacun)
+      buttons[i].addActionListener(new ButtonClickListener());
+
+      if (i == 14) {
+        buttons[i].setForeground(new Color(242, 203, 5));
+        buttons[i].setBackground(new Color(242, 48, 48));
+      } else {
+        buttons[i].setForeground(new Color(242, 159, 5));
+        buttons[i].setBackground(new Color(29, 89, 78));
       }
-
-      /* Imbrication de tout les éléments */
-      globalPanel.add(boardPanel); // Ajoute l'écran à la page
-      globalPanel.add(buttonPanel); // Ajoute "le clavier" à la page
-      window.add(globalPanel); // Ajoute la page à la fenêtre
-      boardPanel.add(calculField); // Ajoute le texte de calcul à l'écran
-      
-      window.setVisible(true);
+      buttonPanel.add(buttons[i]); // Ajoute chaque bouton à la zone buttonPanel
     }
+
+    /* Imbrication de tout les éléments */
+    globalPanel.add(boardPanel); // Ajoute l'écran à la page
+    globalPanel.add(buttonPanel); // Ajoute "le clavier" à la page
+    window.add(globalPanel); // Ajoute la page à la fenêtre
+    boardPanel.add(calculField, BorderLayout.CENTER); // Ajoute le texte de calcul à l'écran
+
+    window.setVisible(true);
+  }
+
+  class ButtonClickListener implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+      String command = e.getActionCommand();
+
+      if (calculField.getText().equals("0")) {
+        calculField.setText(command);
+      } else if (command.matches("[\\d.]")) {
+        calculField.setText(calculField.getText() + command);
+      } else if (command.equals("=")) {
+        calculateResult();
+      } else {
+        calculatePartialValue(command); // When user press "+", "-","x" or "/" button
+      }
+    }
+
+    private void calculateResult() {
+      calculField.setText(Double.toString(currentResult));
+    }
+
+    private void calculatePartialValue(String command) {
+      // TODO Calculate the value each time there's a new operation
+    }
+
+  }
 }
